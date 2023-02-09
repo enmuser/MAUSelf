f07f194584c441380cceacb90af24cc08c0bfeeb
朱洪昌 <1282801732@qq.com>
2023-02-09 09:03:41 +0800

    mau long three

--- core/layers/MAUCell.py
+++ core/layers/MAUCell.py
@@ -57,6 +57,26 @@ class MAUCell(nn.Module):
             nn.LayerNorm([num_hidden, height, width])
         )
 
+        self.conv_t_3 = nn.Sequential(
+            nn.Conv2d(in_channel, 3 * num_hidden, kernel_size=filter_size, stride=stride, padding=self.padding,
+                      ),
+            nn.LayerNorm([3 * num_hidden, height, width])
+        )
+        self.conv_t_next_3 = nn.Sequential(
+            nn.Conv2d(in_channel, num_hidden, kernel_size=filter_size, stride=stride, padding=self.padding,
+                      ),
+            nn.LayerNorm([num_hidden, height, width])
+        )
+        self.conv_s_3 = nn.Sequential(
+            nn.Conv2d(num_hidden, 3 * num_hidden, kernel_size=filter_size, stride=stride, padding=self.padding,
+                      ),
+            nn.LayerNorm([3 * num_hidden, height, width])
+        )
+        self.conv_s_next_3 = nn.Sequential(
+            nn.Conv2d(num_hidden, num_hidden, kernel_size=filter_size, stride=stride, padding=self.padding,
+                      ),
+            nn.LayerNorm([num_hidden, height, width])
+        )
 
         self.softmax = nn.Softmax(dim=0)
 
@@ -81,8 +101,6 @@ class MAUCell(nn.Module):
         S_gate = torch.sigmoid(s_g)
         T_new = T_gate * t_t + (1 - T_gate) * s_t
         S_new = S_gate * s_s + (1 - S_gate) * t_s
-        if self.cell_mode == 'residual':
-            S_new = S_new + S_t
 
         s_next_2 = self.conv_s_next_2(S_t)
         t_next_2 = self.conv_t_next_2(T_t)
@@ -104,9 +122,47 @@ class MAUCell(nn.Module):
         S_gate_2 = torch.sigmoid(s_g_2)
         T_new_2 = T_gate_2 * t_t_2 + (1 - T_gate_2) * s_t_2
         S_new_2 = S_gate_2 * s_s_2 + (1 - S_gate_2) * t_s_2
+
+        s_next_3 = self.conv_s_next_3(S_t)
+        t_next_3 = self.conv_t_next_3(T_t)
+        weights_list_3 = []
+        for i in range(self.tau):
+            weights_list_3.append((s_att[i] * s_next_3).sum(dim=(1, 2, 3)) / math.sqrt(self.d))
+        weights_list_3 = torch.stack(weights_list_3, dim=0)
+        weights_list_3 = torch.reshape(weights_list_3, (*weights_list_3.shape, 1, 1, 1))
+        weights_list_3 = self.softmax(weights_list_3)
+        T_trend_3 = t_att * weights_list_3
+        T_trend_3 = T_trend_3.sum(dim=0)
+        t_att_gate_3 = torch.sigmoid(t_next_3)
+        T_fusion_3 = T_t * t_att_gate_3 + (1 - t_att_gate_3) * T_trend_3
+        T_concat_3 = self.conv_t_3(T_fusion_3)
+        S_concat_3 = self.conv_s_3(S_t)
+        t_g_3, t_t_3, t_s_3 = torch.split(T_concat_3, self.num_hidden, dim=1)
+        s_g_3, s_t_3, s_s_3 = torch.split(S_concat_3, self.num_hidden, dim=1)
+        T_gate_3 = torch.sigmoid(t_g_3)
+        S_gate_3 = torch.sigmoid(s_g_3)
+        T_new_3 = T_gate_3 * t_t_3 + (1 - T_gate_3) * s_t_3
+        S_new_3 = S_gate_3 * s_s_3 + (1 - S_gate_3) * t_s_3
+
+
         # if self.cell_mode == 'residual':
         #     S_new = S_new + S_t
-        T_new = T_new + T_new_2
-        S_new = S_new + S_new_2
+        #T_new_gate = torch.sigmoid(T_new)
+        T_new_2_gate = torch.sigmoid(T_new_2)
+        T_new_3_gate = torch.sigmoid(T_new_3)
 
-        return T_new, S_new
+        T_new_2 = T_new_2 * T_new_2_gate + T_new * (1 - T_new_2_gate)
+        T_new_return = T_new_3 * T_new_3_gate + T_new_2 * (1 - T_new_3_gate)
+
+        #T_new_return = T_new_3
+
+        #S_new_gate = torch.sigmoid(S_new)
+        S_new_2_gate = torch.sigmoid(S_new_2)
+        S_new_3_gate = torch.sigmoid(S_new_3)
+
+        S_new_2 = S_new_2 * S_new_2_gate + S_new * (1 - S_new_2_gate)
+        S_new_return = S_new_3 * S_new_3_gate + S_new_2 * (1 - S_new_3_gate)
+
+        #S_new_return =S_new_3
+
+        return T_new_return, S_new_return
