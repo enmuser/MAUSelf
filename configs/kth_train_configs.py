fb20484a600c173b035407d6af144ba29f4b63fe
朱洪昌 <1282801732@qq.com>
2023-02-26 22:15:02 +0800

    Update kth_train_configs.py

--- configs/kth_train_configs.py
+++ configs/kth_train_configs.py
@@ -7,10 +7,9 @@ import argparse
 def configs():
     parser = argparse.ArgumentParser(description='MAU_train')
 
-
-    parser.add_argument('--data_train_path', type=str, default='/kaggle/input/kthdataset/')
-    parser.add_argument('--data_val_path', type=str, default='/kaggle/input/kthdataset/')
-    parser.add_argument('--data_test_path', type=str, default='/kaggle/input/kthdataset/')
+    parser.add_argument('--data_train_path', type=str, default='/kaggle/input/kthdataset/KTH_64/')
+    parser.add_argument('--data_val_path', type=str, default='/kaggle/input/kthdataset/KTH_64/')
+    parser.add_argument('--data_test_path', type=str, default='/kaggle/input/kthdataset/KTH_64/')
     parser.add_argument('--input_length', type=int, default=10)
     parser.add_argument('--real_length', type=int, default=30)
     parser.add_argument('--total_length', type=int, default=30)
