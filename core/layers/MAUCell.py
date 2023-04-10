a158f98f2304e14618b799fb882041345eb2178c
朱洪昌 <1282801732@qq.com>
2023-04-10 20:52:47 +0800

    cache cross

--- core/layers/MAUCell.py
+++ core/layers/MAUCell.py
@@ -4,7 +4,7 @@ import math
 
 
 class MAUCell(nn.Module):
-    def __init__(self, in_channel, num_hidden, height, width, filter_size, stride, tau, cell_mode):
+    def __init__(self, in_channel, num_hidden, height, width, filter_size, stride, tau, cell_mode,device):
         super(MAUCell, self).__init__()
 
         self.num_hidden = num_hidden
@@ -13,6 +13,7 @@ class MAUCell(nn.Module):
         self.d = num_hidden * height * width
         self.tau = tau
         self.states = ['residual', 'normal']
+        self.device = device
         if not self.cell_mode in self.states:
             raise AssertionError
         self.conv_t = nn.Sequential(
@@ -78,17 +79,70 @@ class MAUCell(nn.Module):
             nn.LayerNorm([num_hidden, height, width])
         )
 
+        # self.conv_t_lower_1 = nn.Sequential(
+        #     nn.Conv2d(in_channel, num_hidden, kernel_size=5, stride=1, padding=2,
+        #               ),
+        #     nn.LayerNorm([num_hidden, height, width])
+        # )
+        # self.conv_t_lower_2 = nn.Sequential(
+        #     nn.Conv2d(in_channel, num_hidden, kernel_size=3, stride=1, padding=1,
+        #               ),
+        #     nn.LayerNorm([num_hidden, height, width])
+        # )
+        # self.conv_s_lower_1 = nn.Sequential(
+        #     nn.Conv2d(in_channel, num_hidden, kernel_size=5, stride=1, padding=2,
+        #               ),
+        #     nn.LayerNorm([num_hidden, height, width])
+        # )
+        # self.conv_s_lower_2 = nn.Sequential(
+        #     nn.Conv2d(in_channel, num_hidden, kernel_size=3, stride=1, padding=1,
+        #               ),
+        #     nn.LayerNorm([num_hidden, height, width])
+        # )
+        #
+        # self.conv_t_upper_1 = nn.Sequential(
+        #     nn.Conv2d(in_channel, num_hidden, kernel_size=5, stride=1, padding=2,
+        #               ),
+        #     nn.LayerNorm([num_hidden, height, width])
+        # )
+        # self.conv_t_upper_2 = nn.Sequential(
+        #     nn.Conv2d(in_channel, num_hidden, kernel_size=7, stride=1, padding=3,
+        #               ),
+        #     nn.LayerNorm([num_hidden, height, width])
+        # )
+        # self.conv_s_upper_1 = nn.Sequential(
+        #     nn.Conv2d(in_channel, num_hidden, kernel_size=5, stride=1, padding=2,
+        #               ),
+        #     nn.LayerNorm([num_hidden, height, width])
+        # )
+        # self.conv_s_upper_2 = nn.Sequential(
+        #     nn.Conv2d(in_channel, num_hidden, kernel_size=7, stride=1, padding=3,
+        #               ),
+        #     nn.LayerNorm([num_hidden, height, width])
+        # )
+
         self.softmax = nn.Softmax(dim=0)
 
     def forward(self, T_t, S_t, t_att, s_att):
-        s_next = self.conv_s_next(S_t)
-        t_next = self.conv_t_next(T_t)
+        T_new_return = []
+        S_new_return = []
+        ##############################111111111111111111111111#####################################
+        current_t_att = []
+        for i in range(self.tau):
+            current_t_att.append(t_att[i][0])
+        current_t_att = torch.stack(current_t_att, dim=0)
+        current_s_att = []
+        for i in range(self.tau):
+            current_s_att.append(s_att[i][0])
+        current_s_att = torch.stack(current_s_att, dim=0)
+        s_next = self.conv_s_next(S_t[0])
+        t_next = self.conv_t_next(T_t[0])
         weights_list_s = []
         weights_list_t = []
         for i in range(self.tau):
-            weights_list_s.append((s_att[i] * s_next).sum(dim=(1, 2, 3)) / math.sqrt(self.d))
+            weights_list_s.append((s_att[i][0] * s_next).sum(dim=(1, 2, 3)) / math.sqrt(self.d))
         for i in range(self.tau):
-            weights_list_t.append((t_att[i] * t_next).sum(dim=(1, 2, 3)) / math.sqrt(self.d))
+            weights_list_t.append((t_att[i][0] * t_next).sum(dim=(1, 2, 3)) / math.sqrt(self.d))
         weights_list_s = torch.stack(weights_list_s, dim=0)
         weights_list_s = torch.reshape(weights_list_s, (*weights_list_s.shape, 1, 1, 1))
         weights_list_s = self.softmax(weights_list_s)
@@ -97,16 +151,16 @@ class MAUCell(nn.Module):
         weights_list_t = torch.reshape(weights_list_t, (*weights_list_t.shape, 1, 1, 1))
         weights_list_t = self.softmax(weights_list_t)
 
-        T_trend = t_att * weights_list_s
+        T_trend = current_t_att * weights_list_s
         T_trend = T_trend.sum(dim=0)
         t_att_gate = torch.sigmoid(t_next)
-        T_fusion = T_t * t_att_gate + (1 - t_att_gate) * T_trend
+        T_fusion = T_t[0] * t_att_gate + (1 - t_att_gate) * T_trend
         T_concat = self.conv_t(T_fusion)
 
-        S_trend = s_att * weights_list_t
+        S_trend = current_s_att * weights_list_t
         S_trend = S_trend.sum(dim=0)
         s_att_gate = torch.sigmoid(s_next)
-        S_fusion = S_t * s_att_gate + (1 - s_att_gate) * S_trend
+        S_fusion = S_t[0] * s_att_gate + (1 - s_att_gate) * S_trend
         S_concat = self.conv_s(S_fusion)
 
         #S_concat = self.conv_s(S_t)
@@ -117,16 +171,25 @@ class MAUCell(nn.Module):
         T_new = T_gate * t_t + (1 - T_gate) * s_t
         S_new = S_gate * s_s + (1 - S_gate) * t_s
 
-        ###################################################################
+        ##############################2222222222222222222222#####################################
 
-        s_next_2 = self.conv_s_next_2(S_t)
-        t_next_2 = self.conv_t_next_2(T_t)
+        current_t_att_2 = []
+        for i in range(self.tau):
+            current_t_att_2.append(t_att[i][1])
+        current_t_att_2 = torch.stack(current_t_att_2, dim=0)
+        current_s_att_2 = []
+        for i in range(self.tau):
+            current_s_att_2.append(s_att[i][1])
+        current_s_att_2 = torch.stack(current_s_att_2, dim=0)
+
+        s_next_2 = self.conv_s_next_2(S_t[1])
+        t_next_2 = self.conv_t_next_2(T_t[1])
         weights_list_s_2 = []
         weights_list_t_2 = []
         for i in range(self.tau):
-            weights_list_s_2.append((s_att[i] * s_next_2).sum(dim=(1, 2, 3)) / math.sqrt(self.d))
+            weights_list_s_2.append((s_att[i][1] * s_next_2).sum(dim=(1, 2, 3)) / math.sqrt(self.d))
         for i in range(self.tau):
-            weights_list_t_2.append((t_att[i] * t_next_2).sum(dim=(1, 2, 3)) / math.sqrt(self.d))
+            weights_list_t_2.append((t_att[i][1] * t_next_2).sum(dim=(1, 2, 3)) / math.sqrt(self.d))
         weights_list_s_2 = torch.stack(weights_list_s_2, dim=0)
         weights_list_s_2 = torch.reshape(weights_list_s_2, (*weights_list_s_2.shape, 1, 1, 1))
         weights_list_s_2 = self.softmax(weights_list_s_2)
@@ -135,16 +198,16 @@ class MAUCell(nn.Module):
         weights_list_t_2 = torch.reshape(weights_list_t_2, (*weights_list_t_2.shape, 1, 1, 1))
         weights_list_t_2 = self.softmax(weights_list_t_2)
 
-        T_trend_2 = t_att * weights_list_s_2
+        T_trend_2 = current_t_att_2 * weights_list_s_2
         T_trend_2 = T_trend_2.sum(dim=0)
         t_att_gate_2 = torch.sigmoid(t_next_2)
-        T_fusion_2 = T_t * t_att_gate_2 + (1 - t_att_gate_2) * T_trend_2
+        T_fusion_2 = T_t[1] * t_att_gate_2 + (1 - t_att_gate_2) * T_trend_2
         T_concat_2 = self.conv_t_2(T_fusion_2)
 
-        S_trend_2 = s_att * weights_list_t_2
+        S_trend_2 = current_s_att_2 * weights_list_t_2
         S_trend_2 = S_trend_2.sum(dim=0)
         s_att_gate_2 = torch.sigmoid(s_next_2)
-        S_fusion_2 = S_t * s_att_gate_2 + (1 - s_att_gate_2) * S_trend_2
+        S_fusion_2 = S_t[1] * s_att_gate_2 + (1 - s_att_gate_2) * S_trend_2
         S_concat_2 = self.conv_s_2(S_fusion_2)
 
         #S_concat_2 = self.conv_s_2(S_t)
@@ -157,14 +220,23 @@ class MAUCell(nn.Module):
 
         ###################################################################
 
-        s_next_3 = self.conv_s_next_3(S_t)
-        t_next_3 = self.conv_t_next_3(T_t)
+        current_t_att_3 = []
+        for i in range(self.tau):
+            current_t_att_3.append(t_att[i][2])
+        current_t_att_3 = torch.stack(current_t_att_3, dim=0)
+        current_s_att_3 = []
+        for i in range(self.tau):
+            current_s_att_3.append(s_att[i][2])
+        current_s_att_3 = torch.stack(current_s_att_3, dim=0)
+
+        s_next_3 = self.conv_s_next_3(S_t[2])
+        t_next_3 = self.conv_t_next_3(T_t[2])
         weights_list_s_3 = []
         weights_list_t_3 = []
         for i in range(self.tau):
-            weights_list_s_3.append((s_att[i] * s_next_3).sum(dim=(1, 2, 3)) / math.sqrt(self.d))
+            weights_list_s_3.append((s_att[i][2] * s_next_3).sum(dim=(1, 2, 3)) / math.sqrt(self.d))
         for i in range(self.tau):
-            weights_list_t_3.append((t_att[i] * t_next_3).sum(dim=(1, 2, 3)) / math.sqrt(self.d))
+            weights_list_t_3.append((t_att[i][2] * t_next_3).sum(dim=(1, 2, 3)) / math.sqrt(self.d))
 
         weights_list_s_3 = torch.stack(weights_list_s_3, dim=0)
         weights_list_s_3 = torch.reshape(weights_list_s_3, (*weights_list_s_3.shape, 1, 1, 1))
@@ -174,19 +246,18 @@ class MAUCell(nn.Module):
         weights_list_t_3 = torch.reshape(weights_list_t_3, (*weights_list_t_3.shape, 1, 1, 1))
         weights_list_t_3 = self.softmax(weights_list_t_3)
 
-        T_trend_3 = t_att * weights_list_s_3
+        T_trend_3 = current_t_att_3 * weights_list_s_3
         T_trend_3 = T_trend_3.sum(dim=0)
         t_att_gate_3 = torch.sigmoid(t_next_3)
-        T_fusion_3 = T_t * t_att_gate_3 + (1 - t_att_gate_3) * T_trend_3
+        T_fusion_3 = T_t[2] * t_att_gate_3 + (1 - t_att_gate_3) * T_trend_3
         T_concat_3 = self.conv_t_3(T_fusion_3)
 
-        S_trend_3 = s_att * weights_list_t_3
+        S_trend_3 = current_s_att_3 * weights_list_t_3
         S_trend_3 = S_trend_3.sum(dim=0)
         s_att_gate_3 = torch.sigmoid(s_next_3)
-        S_fusion_3 = S_t * s_att_gate_3 + (1 - s_att_gate_3) * S_trend_3
+        S_fusion_3 = S_t[2] * s_att_gate_3 + (1 - s_att_gate_3) * S_trend_3
         S_concat_3 = self.conv_s_3(S_fusion_3)
 
-        #S_concat_3 = self.conv_s_3(S_t)
         t_g_3, t_t_3, t_s_3 = torch.split(T_concat_3, self.num_hidden, dim=1)
         s_g_3, s_t_3, s_s_3 = torch.split(S_concat_3, self.num_hidden, dim=1)
         T_gate_3 = torch.sigmoid(t_g_3)
@@ -194,25 +265,53 @@ class MAUCell(nn.Module):
         T_new_3 = T_gate_3 * t_t_3 + (1 - T_gate_3) * s_t_3
         S_new_3 = S_gate_3 * s_s_3 + (1 - S_gate_3) * t_s_3
 
+        # T_new_3_upper = self.conv_t_upper_1(T_new_3)
+        # T_new_2_gate = torch.sigmoid(T_new_2)
+        # T_new_2 = T_new_2 * T_new_2_gate + (1 - T_new_2_gate) * T_new_3_upper
+        # T_new_2_upper = self.conv_t_upper_2(T_new_2)
+        # T_new_gate = torch.sigmoid(T_new)
+        # T_new = T_new * T_new_gate + (1 - T_new_gate) * T_new_2_upper
+        #
+        # T_new_lower = self.conv_t_lower_1(T_new)
+        # T_new_2_gate = torch.sigmoid(T_new_2)
+        # T_new_2 = T_new_2 * T_new_2_gate + (1 - T_new_2_gate) * T_new_lower
+        # T_new_2_lower = self.conv_t_lower_2(T_new_2)
+        # T_new_3_gate = torch.sigmoid(T_new_3)
+        # T_new_3 = T_new_3 * T_new_3_gate + (1 - T_new_3_gate) * T_new_2_lower
+        #
+        # S_new_3_upper = self.conv_s_upper_1(S_new_3)
+        # S_new_2_gate = torch.sigmoid(S_new_2)
+        # S_new_2 = S_new_2 * S_new_2_gate + (1 - S_new_2_gate) * S_new_3_upper
+        # S_new_2_upper = self.conv_s_upper_2(S_new_2)
+        # S_new_gate = torch.sigmoid(S_new)
+        # S_new = S_new * S_new_gate + (1 - S_new_gate) * S_new_2_upper
+        #
+        # S_new_lower = self.conv_s_lower_1(S_new)
+        # S_new_2_gate = torch.sigmoid(S_new_2)
+        # S_new_2 = S_new_2 * S_new_2_gate + (1 - S_new_2_gate) * S_new_lower
+        # S_new_2_lower = self.conv_s_lower_2(S_new_2)
+        # S_new_3_gate = torch.sigmoid(S_new_3)
+        # S_new_3 = S_new_3 * S_new_3_gate + (1 - S_new_3_gate) * S_new_2_lower
 
-        # if self.cell_mode == 'residual':
-        #     S_new = S_new + S_t
-        #T_new_gate = torch.sigmoid(T_new)
         T_new_2_gate = torch.sigmoid(T_new_2)
         T_new_3_gate = torch.sigmoid(T_new_3)
 
         T_new_2 = T_new_2 * T_new_2_gate + T_new * (1 - T_new_2_gate)
-        T_new_return = T_new_3 * T_new_3_gate + T_new_2 * (1 - T_new_3_gate)
+        T_new_3 = T_new_3 * T_new_3_gate + T_new_2 * (1 - T_new_3_gate)
 
-        #T_new_return = T_new_3
+        # T_new_return = T_new_3
 
-        #S_new_gate = torch.sigmoid(S_new)
+        # S_new_gate = torch.sigmoid(S_new)
         S_new_2_gate = torch.sigmoid(S_new_2)
         S_new_3_gate = torch.sigmoid(S_new_3)
 
         S_new_2 = S_new_2 * S_new_2_gate + S_new * (1 - S_new_2_gate)
-        S_new_return = S_new_3 * S_new_3_gate + S_new_2 * (1 - S_new_3_gate)
+        S_new_3 = S_new_3 * S_new_3_gate + S_new_2 * (1 - S_new_3_gate)
 
-        #S_new_return =S_new_3
-
+        T_new_return.append(T_new)
+        T_new_return.append(T_new_2)
+        T_new_return.append(T_new_3)
+        S_new_return.append(S_new)
+        S_new_return.append(S_new_2)
+        S_new_return.append(S_new_3)
         return T_new_return, S_new_return
---
+++ core/models/DCGAN_Conv.py
@@ -0,0 +1,164 @@
+"""
+DCGAN-64 Encoder and Decoder, but stopped after Conv_3.
+These modules are meant to be used along with the ConvLSTMs.
+"""
+
+import torch
+import torch.nn as nn
+
+BN_TRACK_STATS = False
+
+class dcgan_conv(nn.Module):
+    def __init__(self, nin, nout):
+        super().__init__() # 1. nin = 3, nout = 64  2. nin
+        self.main = nn.Sequential(
+                nn.Conv2d(nin, nout, 4, 2, 1),# kernel_size 减少一半
+                nn.BatchNorm2d(nout, track_running_stats=BN_TRACK_STATS),
+                nn.LeakyReLU(0.2, inplace=True),
+                )
+
+    def forward(self, input_):
+        return self.main(input_)
+
+
+class dcgan_upconv(nn.Module):
+    def __init__(self, nin, nout):
+        super().__init__()
+        self.main = nn.Sequential(
+                nn.ConvTranspose2d(nin, nout, 4, 2, 1), # kernel_size 扩大一倍
+                nn.BatchNorm2d(nout, track_running_stats=BN_TRACK_STATS),
+                nn.LeakyReLU(0.2, inplace=True),
+            )
+
+    def forward(self, input_):
+        return self.main(input_)
+
+
+class encoder(nn.Module):
+    def __init__(self, dim, nc=1, nf=64, extra_deep=False):
+        super().__init__() # dim =512, nc = 3, nf = 64, extra_deep = False
+        self.num_blocks = 4
+        self.dim = dim # dim = 512
+        # input is (nc) x 64 x 64
+        self.c1 = dcgan_conv(nc, nf) # (3,64)
+        # state size. (nf) x 32 x 32
+        self.c2 = dcgan_conv(nf, nf * 2) # (64,128)
+        # state size. (nf*2) x 16 x 16
+        self.c3 = dcgan_conv(nf * 2, nf * 4) # (128,256)
+        # state size. (nf*4) x 8 x 8
+        self.c4 = nn.Sequential(
+                nn.Conv2d(nf * 4, dim, 4, 2, 1), # (256,512)
+                nn.BatchNorm2d(dim, track_running_stats=BN_TRACK_STATS),
+                nn.Tanh()
+            )
+        # state size. (dim) x 4 x 4
+
+    def get_spatial_dims(self, img_size, level=-1):
+        if level == -1:
+            level = self.num_blocks - 1
+        assert level in range(self.num_blocks)
+        H, W = img_size
+        scale = 2**(level+1) # level =[1,2,3] =>  scale =[4,8,16]
+        return (H//scale, W//scale) # [(16,16),(8,8),(4,4)]
+
+    def forward(self, input_): # 16 * 3 * 64 * 64
+        h1 = self.c1(input_) # h1 = 16 * 64 * 32 * 32
+        h2 = self.c2(h1) # h2 = 16 * 128 * 16 * 16
+        h3 = self.c3(h2) # h3 = 16 * 256 * 8 * 8
+        h4 = self.c4(h3) # h4 = 16 * 512 * 4 * 4
+        return h4, [h1, h2, h3] # 16 * 512 * 4 * 4,[16 * 64 * 32 * 32,16 * 128 * 16 * 16, 16 * 256 * 8 * 8]
+
+
+class decoder(nn.Module):
+    def __init__(self, dim, nc=1, nf=64, extra_deep=False):
+        super().__init__()
+        self.dim = dim # dim = 512
+        # state size. (dim) x 4 x 4
+        self.upc1 = dcgan_upconv(dim, nf * 4) # (512,256)
+        # state size. (nf * 2) x 8 x 8
+        self.upc2 = dcgan_upconv(nf * 4 * 2, nf * 2) # (512,128)
+        # state size. (nf*2) x 16 x 16
+        self.upc3 = dcgan_upconv(nf * 2 * 2, nf)  # (256,64)
+        # state size. (nf*2) x 32 x 32
+        self.upc4 = nn.Sequential(
+                nn.ConvTranspose2d(nf * 2, nc, 4, 2, 1), # (128,64)
+                nn.Sigmoid()
+            )
+        # state size. (nf) x 64 x 64
+        return
+
+    def forward(self, input_):
+        vec, skips = input_ # vec = 16 * 512 * 4 * 4, skips = [16 * 64 * 32 * 32, 16 * 128 * 16 * 16, 16 * 256 * 8 * 8]
+        d1 = self.upc1(vec) # d1 = 16 * 256 * 8 * 8 <= 16 * 512 * 4 * 4 d1
+        d2 = self.upc2(torch.cat([d1, skips[-1]], 1)) # d2 = 16 * 128 * 16 * 16 <= cat(16 * 256 * 8 * 8 , 16 * 256 * 8 * 8) = 16 * 512 * 8 * 8
+        d3 = self.upc3(torch.cat([d2, skips[-2]], 1)) # d3 = 16 * 64 * 32 * 32  <= cat(16 * 128 * 16 * 16, 16 * 128 * 16 * 16) = 16 * 256 * 16 * 16
+        output = self.upc4(torch.cat([d3, skips[-3]], 1)) # output = 16 * 3 * 64 * 64  <= cat(16 * 64 * 32 * 32, 16 * 64 * 32 * 32) = 16 * 128 * 32 * 32
+        return output, [d1, d2, d3] # 16 * 3 * 64 * 64,[16 * 256 * 8 * 8,16 * 128 * 16 * 16,16 * 64 * 32 * 32]
+
+
+class encoder_self(nn.Module):
+    def __init__(self, in_channel=64, num_hidden=64, height=16, width=16):
+        super().__init__() # dim =512, nc = 3, nf = 64, extra_deep = False
+        self.c1 = nn.Sequential(
+            nn.Conv2d(in_channel, num_hidden, kernel_size=7, stride=1, padding=3,),
+            nn.LayerNorm([num_hidden, height, width])
+        )
+        self.c2 = nn.Sequential(
+            nn.Conv2d(in_channel, num_hidden, kernel_size=5, stride=1, padding=2,),
+            nn.LayerNorm([num_hidden, height, width])
+        )
+        self.c3 = nn.Sequential(
+            nn.Conv2d(in_channel, num_hidden, kernel_size=3, stride=1, padding=1, ),
+            nn.LayerNorm([num_hidden, height, width])
+        )
+        self.c4 = nn.Sequential(
+            nn.Conv2d(in_channel, num_hidden, kernel_size=3, stride=1, padding=1, ),
+            nn.LayerNorm([num_hidden, height, width])
+        )
+
+    def get_spatial_dims(self, img_size, level=-1):
+        if level == -1:
+            level = self.num_blocks - 1
+        assert level in range(self.num_blocks)
+        H, W = img_size
+        scale = 2**(level+1) # level =[1,2,3] =>  scale =[4,8,16]
+        return (H//scale, W//scale) # [(16,16),(8,8),(4,4)]
+
+    def forward(self, input_): # 16 * 3 * 64 * 64
+        h1 = self.c1(input_) # h1 = 16 * 64 * 32 * 32
+        h2 = self.c2(h1) # h2 = 16 * 128 * 16 * 16
+        h3 = self.c3(h2) # h3 = 16 * 256 * 8 * 8
+        h4 = self.c3(h3)  # h3 = 16 * 256 * 8 * 8
+        return [h1, h2, h3], h4 # 16 * 512 * 4 * 4,[16 * 64 * 32 * 32,16 * 128 * 16 * 16, 16 * 256 * 8 * 8]
+
+
+class decoder_self(nn.Module):
+    def __init__(self, in_channel=64, num_hidden=64, height=16, width=16):
+        super().__init__()
+
+        self.upc1 = nn.Sequential(
+            nn.Conv2d(in_channel, num_hidden, kernel_size=3, stride=1, padding=1, ),
+            nn.LayerNorm([num_hidden, height, width])
+        )
+        self.upc2 = nn.Sequential(
+            nn.Conv2d(in_channel * 2, num_hidden, kernel_size=7, stride=1, padding=3, ),
+            nn.LayerNorm([num_hidden, height, width])
+        )
+        self.upc3 = nn.Sequential(
+            nn.Conv2d(in_channel * 2, num_hidden, kernel_size=5, stride=1, padding=2,),
+            nn.LayerNorm([num_hidden, height, width])
+        )
+        self.upc4 = nn.Sequential(
+            nn.Conv2d(in_channel * 2, num_hidden, kernel_size=3, stride=1, padding=1,),
+            nn.LayerNorm([num_hidden, height, width])
+        )
+
+    def forward(self, input_):
+        vec, skips = input_ # vec = 16 * 512 * 4 * 4, skips = [16 * 64 * 32 * 32, 16 * 128 * 16 * 16, 16 * 256 * 8 * 8]
+        d1 = self.upc1(vec) # d1 = 16 * 256 * 8 * 8 <= 16 * 512 * 4 * 4 d1
+        d2 = self.upc2(torch.cat([d1, skips[-3]], 1)) # d2 = 16 * 128 * 16 * 16 <= cat(16 * 256 * 8 * 8 , 16 * 256 * 8 * 8) = 16 * 512 * 8 * 8
+        d3 = self.upc3(torch.cat([d2, skips[-2]], 1)) # d3 = 16 * 64 * 32 * 32  <= cat(16 * 128 * 16 * 16, 16 * 128 * 16 * 16) = 16 * 256 * 16 * 16
+        output = self.upc4(torch.cat([d3, skips[-1]], 1)) # output = 16 * 3 * 64 * 64  <= cat(16 * 64 * 32 * 32, 16 * 64 * 32 * 32) = 16 * 128 * 32 * 32
+        return output, [d1, d2, d3] # 16 * 3 * 64 * 64,[16 * 256 * 8 * 8,16 * 128 * 16 * 16,16 * 64 * 32 * 32]
+
+#
