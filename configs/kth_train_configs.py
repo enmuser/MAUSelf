d6caf14ffcc226d1b9d2f7bcbd2b80b763d76be4
朱洪昌 <1282801732@qq.com>
2023-04-25 19:30:43 +0800

    Update kth_train_configs.py

--- configs/kth_train_configs.py
+++ configs/kth_train_configs.py
@@ -42,10 +42,10 @@ def configs():
     parser.add_argument('--display_interval', type=int, default=1)
     parser.add_argument('--test_interval', type=int, default=1000)
     parser.add_argument('--snapshot_interval', type=int, default=1000)
-    parser.add_argument('--num_save_samples', type=int, default=5)
+    parser.add_argument('--num_save_samples', type=int, default=10)
     parser.add_argument('--n_gpu', type=int, default=1)
     # /kaggle/input/kthdataset128/model.ckpt-78000
-    parser.add_argument('--pretrained_model', type=str, default='/kaggle/input/kthdataset128/model.ckpt-302000')
+    parser.add_argument('--pretrained_model', type=str, default='/kaggle/input/kthdataset128/model.ckpt-306000')
     parser.add_argument('--perforamnce_dir', type=str, default='/kaggle/working/MAUSelf/results/kth/')
     parser.add_argument('--save_dir', type=str, default='/kaggle/working/MAUSelf/checkpoints/kth/')
     parser.add_argument('--gen_frm_dir', type=str, default='/kaggle/working/MAUSelf/results/kth/')
