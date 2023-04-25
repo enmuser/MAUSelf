e0cc35357bd10f120d205bc235ac2e00c2bba4a4
朱洪昌 <1282801732@qq.com>
2023-04-25 19:32:56 +0800

    Update MAU_run.py

--- MAU_run.py
+++ MAU_run.py
@@ -166,7 +166,7 @@ def test_wrapper(model):
                                                        batch_size=args.batch_size,
                                                        split="test",
                                                        is_training=False,
-                                                       is_shuffle=False)
+                                                       is_shuffle=True)
 
     itr = 1
     for i in range(itr):
