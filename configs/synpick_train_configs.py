3d156ac2f95fb48dd19e59c95e474d3bf121a68d
朱洪昌 <1282801732@qq.com>
2023-05-03 11:08:37 +0800

    Update synpick_train_configs.py

--- configs/synpick_train_configs.py
+++ configs/synpick_train_configs.py
@@ -8,9 +8,9 @@ def configs():
     parser = argparse.ArgumentParser(description='MAU_train')
 
 
-    parser.add_argument('--data_train_path', type=str, default='/kaggle/input/synpickdataset/')
-    parser.add_argument('--data_val_path', type=str, default='/kaggle/input/synpickdataset/')
-    parser.add_argument('--data_test_path', type=str, default='/kaggle/input/synpickdataset/')
+    parser.add_argument('--data_train_path', type=str, default='/kaggle/input/synpickdataset/SYNPICK/')
+    parser.add_argument('--data_val_path', type=str, default='/kaggle/input/synpickdataset/SYNPICK/')
+    parser.add_argument('--data_test_path', type=str, default='/kaggle/input/synpickdataset/SYNPICK/')
     parser.add_argument('--input_length', type=int, default=10)
     parser.add_argument('--real_length', type=int, default=20)
     parser.add_argument('--total_length', type=int, default=20)
