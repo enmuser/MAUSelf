fa52a74277afa4e8ac15d5faeec53c0cc4de9afc
朱洪昌 <1282801732@qq.com>
2023-05-30 09:15:10 +0800

    Update kitti_train_configs.py

--- configs/kitti_train_configs.py
+++ configs/kitti_train_configs.py
@@ -47,7 +47,7 @@ def configs():
     parser.add_argument('--snapshot_interval', type=int, default=1000)
     parser.add_argument('--num_save_samples', type=int, default=10)
     parser.add_argument('--n_gpu', type=int, default=1)
-    parser.add_argument('--pretrained_model', type=str, default='/kaggle/input/kittidataset/model.ckpt-16000')
+    parser.add_argument('--pretrained_model', type=str, default='/kaggle/input/kittidataset/model.ckpt-24000')
     parser.add_argument('--perforamnce_dir', type=str, default='/kaggle/working/MAUSelf/results/kitti/')
     parser.add_argument('--save_dir', type=str, default='/kaggle/working/MAUSelf/checkpoints/kitti/')
     parser.add_argument('--gen_frm_dir', type=str, default='/kaggle/working/MAUSelf/results/kitti/')
