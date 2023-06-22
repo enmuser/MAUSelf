a8bb38495efc5c95354d2eea2dac70fbea302f2c
朱洪昌 <1282801732@qq.com>
2023-06-22 15:41:13 +0800

    kittidataset

--- configs/kth_train_configs.py
+++ configs/kth_train_configs.py
@@ -14,6 +14,7 @@ def configs():
     parser.add_argument('--input_length', type=int, default=10)
     parser.add_argument('--real_length', type=int, default=30)
     parser.add_argument('--total_length', type=int, default=30)
+    parser.add_argument('--pred_length', type=int, default=20)
     parser.add_argument('--img_height', type=int, default=128)
     parser.add_argument('--img_width', type=int, default=128)
     parser.add_argument('--img_channel', type=int, default=1)
