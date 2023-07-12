f0832264c9eeffdc79430ed5e77f650375c78ff8
朱洪昌 <1282801732@qq.com>
2023-07-12 22:48:58 +0800

    fb测试输入场景

--- core/models/MAU.py
+++ core/models/MAU.py
@@ -56,6 +56,57 @@ class RNN(nn.Module):
             encoders.append(encoder)
         self.encoders = nn.ModuleList(encoders)
 
+        encoders_mask = []
+        encoder_mask = nn.Sequential()
+        encoder_mask.add_module(name='encoder_t_conv{0}'.format(-1),
+                           module=nn.Conv2d(in_channels=self.frame_channel,
+                                            out_channels=self.num_hidden[0],
+                                            stride=1,
+                                            padding=0,
+                                            kernel_size=1))
+        encoder_mask.add_module(name='relu_t_{0}'.format(-1),
+                           module=nn.LeakyReLU(0.2))
+        encoders_mask.append(encoder_mask)
+        for i in range(n):
+            encoder_mask = nn.Sequential()
+            encoder_mask.add_module(name='encoder_t{0}'.format(i),
+                               module=nn.Conv2d(in_channels=self.num_hidden[0],
+                                                out_channels=self.num_hidden[0],
+                                                stride=(2, 2),
+                                                padding=(1, 1),
+                                                kernel_size=(3, 3)
+                                                ))
+            encoder_mask.add_module(name='encoder_t_relu{0}'.format(i),
+                               module=nn.LeakyReLU(0.2))
+            encoders_mask.append(encoder_mask)
+        self.encoders_mask = nn.ModuleList(encoders_mask)
+
+
+        encoders_back = []
+        encoder_back = nn.Sequential()
+        encoder_back.add_module(name='encoder_t_conv{0}'.format(-1),
+                           module=nn.Conv2d(in_channels=self.frame_channel,
+                                            out_channels=self.num_hidden[0],
+                                            stride=1,
+                                            padding=0,
+                                            kernel_size=1))
+        encoder_back.add_module(name='relu_t_{0}'.format(-1),
+                           module=nn.LeakyReLU(0.2))
+        encoders_back.append(encoder_back)
+        for i in range(n):
+            encoder_back = nn.Sequential()
+            encoder_back.add_module(name='encoder_t{0}'.format(i),
+                               module=nn.Conv2d(in_channels=self.num_hidden[0],
+                                                out_channels=self.num_hidden[0],
+                                                stride=(2, 2),
+                                                padding=(1, 1),
+                                                kernel_size=(3, 3)
+                                                ))
+            encoder_back.add_module(name='encoder_t_relu{0}'.format(i),
+                               module=nn.LeakyReLU(0.2))
+            encoders_back.append(encoder_back)
+        self.encoders_back = nn.ModuleList(encoders_back)
+
         # Decoder
         decoders = []
 
@@ -86,13 +137,75 @@ class RNN(nn.Module):
             decoders.append(decoder)
         self.decoders = nn.ModuleList(decoders)
 
+        decoders_mask = []
+        for i in range(n - 1):
+            decoder_mask = nn.Sequential()
+            decoder_mask.add_module(name='c_decoder{0}'.format(i),
+                               module=nn.ConvTranspose2d(in_channels=self.num_hidden[-1],
+                                                         out_channels=self.num_hidden[-1],
+                                                         stride=(2, 2),
+                                                         padding=(1, 1),
+                                                         kernel_size=(3, 3),
+                                                         output_padding=(1, 1)
+                                                         ))
+            decoder_mask.add_module(name='c_decoder_relu{0}'.format(i),
+                               module=nn.LeakyReLU(0.2))
+            decoders_mask.append(decoder_mask)
+
+        if n > 0:
+            decoder_mask = nn.Sequential()
+            decoder_mask.add_module(name='c_decoder{0}'.format(n - 1),
+                               module=nn.ConvTranspose2d(in_channels=self.num_hidden[-1],
+                                                         out_channels=self.num_hidden[-1],
+                                                         stride=(2, 2),
+                                                         padding=(1, 1),
+                                                         kernel_size=(3, 3),
+                                                         output_padding=(1, 1)
+                                                         ))
+            decoders_mask.append(decoder_mask)
+        self.decoders_mask = nn.ModuleList(decoders_mask)
+
+        decoders_back = []
+        for i in range(n - 1):
+            decoder_back = nn.Sequential()
+            decoder_back.add_module(name='c_decoder{0}'.format(i),
+                               module=nn.ConvTranspose2d(in_channels=self.num_hidden[-1],
+                                                         out_channels=self.num_hidden[-1],
+                                                         stride=(2, 2),
+                                                         padding=(1, 1),
+                                                         kernel_size=(3, 3),
+                                                         output_padding=(1, 1)
+                                                         ))
+            decoder_back.add_module(name='c_decoder_relu{0}'.format(i),
+                               module=nn.LeakyReLU(0.2))
+            decoders_back.append(decoder_back)
+
+        if n > 0:
+            decoder_back = nn.Sequential()
+            decoder_back.add_module(name='c_decoder{0}'.format(n - 1),
+                               module=nn.ConvTranspose2d(in_channels=self.num_hidden[-1],
+                                                         out_channels=self.num_hidden[-1],
+                                                         stride=(2, 2),
+                                                         padding=(1, 1),
+                                                         kernel_size=(3, 3),
+                                                         output_padding=(1, 1)
+                                                         ))
+            decoders_back.append(decoder_back)
+        self.decoders_back = nn.ModuleList(decoders_back)
+
         self.srcnn = nn.Sequential(
+            nn.Conv2d(self.num_hidden[-1], self.frame_channel, kernel_size=1, stride=1, padding=0)
+        )
+        self.srcnn_mask = nn.Sequential(
+            nn.Conv2d(self.num_hidden[-1], self.frame_channel, kernel_size=1, stride=1, padding=0)
+        )
+        self.srcnn_back = nn.Sequential(
             nn.Conv2d(self.num_hidden[-1], self.frame_channel, kernel_size=1, stride=1, padding=0)
         )
         self.merge = nn.Conv2d(self.num_hidden[-1] * 2, self.num_hidden[-1], kernel_size=1, stride=1, padding=0)
         self.conv_last_sr = nn.Conv2d(self.frame_channel * 2, self.frame_channel, kernel_size=1, stride=1, padding=0)
 
-    def forward(self, frames, frames_mask, frames_back, mask_true, itr):
+    def forward(self, frames, frames_mask, frames_back, mask_true,itr):
         # print('ok')
         mask_true = mask_true.permute(0, 1, 4, 2, 3).contiguous()
         batch_size = frames.shape[0]
@@ -148,10 +261,11 @@ class RNN(nn.Module):
                 # net = mask_true[:, time_diff] * frames[:, t] + (1 - mask_true[:, time_diff]) * x_gen
                 # net = mask_true[:, time_diff] * frames[:, t] + (1 - mask_true[:, time_diff]) * x_gen
                 # net_back = frames_back[:, (self.configs.input_length - 1)]
-                if itr <= 8000:
+                # print("Itr: ", itr)
+                if itr <= 20000:
                     net_mask = frames_mask[:, t]
                     net_back = frames_back[:, t]
-                elif itr <= 16000:
+                elif itr <= 40000:
                     list1 = [0,1]
                     random_element = random.choice(list1)
                     if random_element == 0:
@@ -169,6 +283,20 @@ class RNN(nn.Module):
                         else:
                             net_mask = frames_mask[:, t]
                             net_back = frames_back[:, t]
+                elif itr <= 60000:
+                    if ((t+2) % 3) == 0:
+                        net_mask = frames_mask[:, t]
+                        net_back = frames_back[:, t]
+                    else:
+                        net_mask = x_gen_mask
+                        net_back = x_gen_back
+                elif itr <= 80000:
+                    if ((t+2) % 4) == 0:
+                        net_mask = frames_mask[:, t]
+                        net_back = frames_back[:, t]
+                    else:
+                        net_mask = x_gen_mask
+                        net_back = x_gen_back
                 else:
                     net_mask = x_gen_mask
                     net_back = x_gen_back
@@ -184,11 +312,11 @@ class RNN(nn.Module):
             for i in range(len(self.encoders)):
                 frames_feature = self.encoders[i](frames_feature)
                 frames_feature_encoded.append(frames_feature)
-            for i in range(len(self.encoders)):
-                frames_feature_mask = self.encoders[i](frames_feature_mask)
+            for i in range(len(self.encoders_mask)):
+                frames_feature_mask = self.encoders_mask[i](frames_feature_mask)
                 frames_feature_mask_encoded.append(frames_feature_mask)
-            for i in range(len(self.encoders)):
-                frames_feature_back = self.encoders[i](frames_feature_back)
+            for i in range(len(self.encoders_back)):
+                frames_feature_back = self.encoders_back[i](frames_feature_back)
                 frames_feature_back_encoded.append(frames_feature_back)
             if t == 0:
                 for i in range(self.num_layers):
@@ -236,18 +364,18 @@ class RNN(nn.Module):
                 out = self.decoders[i](out)
                 if self.configs.model_mode == 'recall':
                     out = out + frames_feature_encoded[-2 - i]
-            for i in range(len(self.decoders)):
-                out_mask = self.decoders[i](out_mask)
+            for i in range(len(self.decoders_mask)):
+                out_mask = self.decoders_mask[i](out_mask)
                 if self.configs.model_mode == 'recall':
                     out_mask = out_mask + frames_feature_mask_encoded[-2 - i]
-            for i in range(len(self.decoders)):
-                out_back = self.decoders[i](out_back)
+            for i in range(len(self.decoders_back)):
+                out_back = self.decoders_back[i](out_back)
                 if self.configs.model_mode == 'recall':
                     out_back = out_back + frames_feature_back_encoded[-2 - i]
 
             x_gen = self.srcnn(out)
-            x_gen_mask = self.srcnn(out_mask)
-            x_gen_back = self.srcnn(out_back)
+            x_gen_mask = self.srcnn_mask(out_mask)
+            x_gen_back = self.srcnn_back(out_back)
             next_frames.append(x_gen)
         next_frames = torch.stack(next_frames, dim=0).permute(1, 0, 2, 3, 4).contiguous()
         return next_frames
