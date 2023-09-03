fbd36c53cb812c07617558eee91cfcc79dc1c4c7
朱洪昌 <1282801732@qq.com>
2023-09-03 21:56:45 +0800

    Update mnist_train_configs.py

--- configs/mnist_train_configs.py
+++ configs/mnist_train_configs.py
@@ -48,8 +48,8 @@ def configs():
     parser.add_argument('--n_gpu', type=int, default=1)
     #/kaggle/input/movingmnist/model.ckpt-168000
     parser.add_argument('--pretrained_model', type=str, default='')
-    parser.add_argument('--pretrained_model_f', type=str, default='/kaggle/input/movingmnist/model.ckpt-140000')
-    parser.add_argument('--pretrained_model_b', type=str, default='/kaggle/input/movingmnist/model.ckpt-40000')
+    parser.add_argument('--pretrained_model_f', type=str, default='/kaggle/input/movingmnist/model_front.ckpt-140000')
+    parser.add_argument('--pretrained_model_b', type=str, default='/kaggle/input/movingmnist/model_back.ckpt-40000')
     parser.add_argument('--perforamnce_dir', type=str, default='/kaggle/working/MAUSelf/results/mau/')
     parser.add_argument('--save_dir', type=str, default='/kaggle/working/MAUSelf/checkpoints/mau/')
     parser.add_argument('--gen_frm_dir', type=str, default='/kaggle/working/MAUSelf/results/mau/')
