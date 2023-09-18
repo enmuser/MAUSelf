6a459eb7b2e0f22bbca135a2040a9f4bf0606936
朱洪昌 <1282801732@qq.com>
2023-09-18 19:27:22 +0800

    Update MAU.py

--- core/models/MAU.py
+++ core/models/MAU.py
@@ -280,7 +280,7 @@ class RNN(nn.Module):
                     else:
                         net_mask = x_gen_mask
                         net_back = x_gen_back
-                elif itr <= (self.train_level_base_line + 300000):
+                elif itr <= (self.train_level_base_line + 150000):
                     if t <= 15:
                         net_mask = frames_mask[:, t]
                         net_back = frames_back[:, t]
