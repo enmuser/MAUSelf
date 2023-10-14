1242e5fe97f8c0eb7886ef14bce87430e0ff5d70
朱洪昌 <1282801732@qq.com>
2023-10-14 12:22:27 +0800

    Update mm.py

--- core/data_provider/mm.py
+++ core/data_provider/mm.py
@@ -120,7 +120,7 @@ class MovingMNIST(data.Dataset):
             name = str(t) + '.png'
             file_name = os.path.join("/kaggle/working/MAUSelf/results/mau/video/file", name)
             cv2.imwrite(file_name, img.astype(np.uint8))
-        img2video(image_root="results/mau/video/file/", dst_name="/kaggle/working/MAUSelf/results/mau/video/file/images.mp4")
+        img2video(image_root="/kaggle/working/MAUSelf/results/mau/video/file/", dst_name="/kaggle/working/MAUSelf/results/mau/video/file/images.mp4")
         # backSub = cv.createBackgroundSubtractorMOG2()
         backSub = cv.createBackgroundSubtractorKNN()
         capture = cv.VideoCapture(cv.samples.findFileOrKeep("/kaggle/working/MAUSelf/results/mau/video/file/images.mp4"))
