0668f38e1460f880429408c8993d1248aae30145
朱洪昌 <1282801732@qq.com>
2023-10-28 19:57:22 +0800

    Update mnist_train_configs.py

--- configs/mnist_train_configs.py
+++ configs/mnist_train_configs.py
@@ -13,6 +13,7 @@ def configs():
     parser.add_argument('--input_length', type=int, default=10)
     parser.add_argument('--real_length', type=int, default=20)
     parser.add_argument('--total_length', type=int, default=20)
+    parser.add_argument('--pred_length', type=int, default=10)
     parser.add_argument('--img_height', type=int, default=64)
     parser.add_argument('--img_width', type=int, default=64)
     parser.add_argument('--sr_size', type=int, default=4)
