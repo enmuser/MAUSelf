22a2cc6a0ef407259875254c7fa3f324ec40871e
朱洪昌 <1282801732@qq.com>
2023-11-22 19:45:46 +0800

    Update taxibj_train_configs.py

--- configs/taxibj_train_configs.py
+++ configs/taxibj_train_configs.py
@@ -45,7 +45,7 @@ def configs():
     parser.add_argument('--snapshot_interval', type=int, default=1000)
     parser.add_argument('--num_save_samples', type=int, default=5)
     parser.add_argument('--n_gpu', type=int, default=1)
-    parser.add_argument('--pretrained_model', type=str, default='/kaggle/input/taxibjdataset/model.ckpt-29000')
+    parser.add_argument('--pretrained_model', type=str, default='/kaggle/input/taxibjdataset/model.ckpt-43000')
     parser.add_argument('--perforamnce_dir', type=str, default='/kaggle/working/MAUSelf/results/taxibj/')
     parser.add_argument('--save_dir', type=str, default='/kaggle/working/MAUSelf/checkpoints/taxibj/')
     parser.add_argument('--gen_frm_dir', type=str, default='/kaggle/working/MAUSelf/results/taxibj/')
