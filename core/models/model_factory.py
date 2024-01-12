be569363c369286cc0b9394e557d50a76f536243
朱洪昌 <1282801732@qq.com>
2024-01-12 22:59:23 +0800

    Update model_factory.py

--- core/models/model_factory.py
+++ core/models/model_factory.py
@@ -61,10 +61,10 @@ class Model(object):
             next_frames = self.network(frames_tensor, mask_tensor, index)
             ground_truth = frames_tensor
             self.optimizer.zero_grad()
-            loss_l1 = self.L1_loss(next_frames,
-                                   ground_truth[:, 1:index+2])
-            loss_l2 = self.MSE_criterion(next_frames,
-                                         ground_truth[:, 1:index+2])
+            loss_l1 = self.L1_loss(next_frames[:, 10:index+2],
+                                   ground_truth[:, 10:index+2])
+            loss_l2 = self.MSE_criterion(next_frames[:, 10:index+2],
+                                         ground_truth[:, 10:index+2])
             loss_gen = loss_l2
             loss_gen.backward()
             self.optimizer.step()
