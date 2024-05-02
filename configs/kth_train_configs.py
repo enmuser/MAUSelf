9c5915cfda0aa8cea21627b5f3dd2510766902d1
朱洪昌 <1282801732@qq.com>
2024-05-02 20:13:46 +0800

    获取视频测试数据

--- configs/kth_train_configs.py
+++ configs/kth_train_configs.py
@@ -18,7 +18,7 @@ def configs():
     parser.add_argument('--img_height', type=int, default=128)
     parser.add_argument('--img_width', type=int, default=128)
     parser.add_argument('--sr_size', type=int, default=4)
-    parser.add_argument('--img_channel', type=int, default=1)
+    parser.add_argument('--img_channel', type=int, default=3)
     parser.add_argument('--patch_size', type=int, default=1)
     parser.add_argument('--alpha', type=float, default=1)
     parser.add_argument('--model_name', type=str, default='mau')
