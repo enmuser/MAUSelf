fd907812e673589f0fd922db2e38a9439ded0177
朱洪昌 <1282801732@qq.com>
2024-10-27 11:52:08 +0800

    radar训练

--- configs/radar_train_configs.py
+++ configs/radar_train_configs.py
@@ -38,7 +38,7 @@ def configs():
     parser.add_argument('--lr', type=float, default=1e-3)
     parser.add_argument('--lr_decay', type=float, default=0.90)
     parser.add_argument('--delay_interval', type=float, default=2000)
-    parser.add_argument('--batch_size', type=int, default=8)
+    parser.add_argument('--batch_size', type=int, default=6)
     parser.add_argument('--test_batch_size', type=int, default=10)
     parser.add_argument('--max_iterations', type=int, default=18000)
     parser.add_argument('--max_epoches', type=int, default=18000)
